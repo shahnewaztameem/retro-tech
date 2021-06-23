1c44caffafc3f088e6de95afd657d5b3ac1e53c8
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-23 19:57:58 +0600

    Added user and post model

---
+++ backend/models/postModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const postSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Post = mongoose.Model('Post', postSchema)
+
+export default Post
