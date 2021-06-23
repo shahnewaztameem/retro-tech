2d964ddb409196406f0c58d662797ca21556e239
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-24 01:11:24 +0600

    Implement Verify jwt token and user login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { authUser } from '../controller/userController.js'
+import { authUser, getUserProfile } from '../controller/userController.js'
+import { ensureTokenIsValid } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/login').post(authUser)
-
+router.route('/profile').get(ensureTokenIsValid, getUserProfile)
 export default router
