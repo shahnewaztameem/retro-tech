5182e6dd955f33f44b9ea38df7c78460ab183640
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-24 00:24:09 +0600

    User controller added

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import express from 'express'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import postRoutes from './routes/postRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
@@ -17,13 +20,13 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/posts', postRoutes)
+app.use('/api/users', userRoutes)
 
 // 404 routing error handler
 app.use(notFound)
 
 // custom error handler
 app.use(errorHandler)
-
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
