46b1ab9e56f2b6ceb0c93ed8444d2d1dfebe2af6
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-25 01:35:03 +0600

    Login redirect and logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
   return (
     <div>
-      header
+      {userInfo ? (
+        <div>
+          <Link to='/profile'>{userInfo.name}</Link>
+          <button onClick={logoutHandler}>Logout</button>
+        </div>
+      ) : (
+        <Link to='/login'>Login</Link>
+      )}
     </div>
   )
 }
