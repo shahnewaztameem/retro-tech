3149639d452483bdf220264c0de2a18eb2006582
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-25 15:27:34 +0600

    Admin middleware added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -28,4 +28,13 @@ const ensureTokenIsValid = asyncHandler(async (req, res, next) => {
   }
 })
 
-export { ensureTokenIsValid }
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+}
+
+export { ensureTokenIsValid, isAdmin }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { authUser, getUserProfile } from '../controller/userController.js'
-import { ensureTokenIsValid } from '../middleware/authMiddleware.js'
+import { ensureTokenIsValid, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/login').post(authUser)
-router.route('/profile').get(ensureTokenIsValid, getUserProfile)
+router.route('/profile').get(ensureTokenIsValid, isAdmin, getUserProfile)
 export default router
