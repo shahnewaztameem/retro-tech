6517242e6c51647401bac8cfdc1bb3a48e6e2b5a
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-26 20:59:33 +0600

    Process worker and post build script added

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,24 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from API' })
-})
-
 app.use('/api/posts', postRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/upload', uploadRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.json({ message: 'Hello from API' })
+  })
+}
 
 // 404 routing error handler
 app.use(notFound)
