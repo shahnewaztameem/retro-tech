712cf8ebd4216dec0b618e76fa089655ea5c3900
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-06-26 15:15:04 +0600

    Add and update post endpoint implement

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -42,4 +42,42 @@ const deletePost = asyncHandler(async (req, res) => {
     throw new Error('Post not found')
   }
 })
-export { getAllPosts, getPost, deletePost }
+
+// @desc    Create single post
+// @route   POST /api/posts
+// @access  Private/admin
+const createPost = asyncHandler(async (req, res) => {
+  const post = new Post({
+    user: req.user._id,
+    name: 'Sample post',
+    image: '/images/sample.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem assumenda eum possimus, repellendus vero sint ipsa rem in vitae ullam.',
+  })
+
+  const createdPost = await post.save()
+  res.status(201).json(createdPost)
+})
+
+// @desc    Update single post
+// @route   PUT /api/posts/:id
+// @access  Private/admin
+const updatePost = asyncHandler(async (req, res) => {
+  const { name, image, description } = req.body
+
+  const post = await Post.findById(req.params.id)
+
+  if (post) {
+    post.name = name
+    post.image = image
+    post.description = description
+
+    const updatedPost = await post.save()
+    res.json(updatedPost)
+  } else {
+    res.status(404)
+    throw new Error('Post not found!')
+  }
+})
+
+export { getAllPosts, getPost, deletePost, createPost, updatePost }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,17 +1,20 @@
 import express from 'express'
 import {
+  createPost,
   deletePost,
   getAllPosts,
   getPost,
+  updatePost,
 } from '../controller/postController.js'
 import { ensureTokenIsValid, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').get(getAllPosts)
+router.route('/').get(getAllPosts).post(ensureTokenIsValid, isAdmin, createPost)
 router
   .route('/:id')
   .get(getPost)
+  .put(ensureTokenIsValid, isAdmin, updatePost)
   .delete(ensureTokenIsValid, isAdmin, deletePost)
 
 export default router
